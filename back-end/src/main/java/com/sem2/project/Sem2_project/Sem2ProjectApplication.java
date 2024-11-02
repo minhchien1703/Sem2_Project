d47ed04bba35f66d5cbf7f8f3d2cd4d0905c8df7
CHIEN MINH <chienminh1703@gmail.com>
2024-11-02 15:59:49 +0700

    update folder back-end

---
+++ back-end/src/main/java/com/sem2/project/Sem2_project/Sem2ProjectApplication.java
@@ -0,0 +1,13 @@
+package com.sem2.project.Sem2_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sem2ProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sem2ProjectApplication.class, args);
+	}
+
+}
